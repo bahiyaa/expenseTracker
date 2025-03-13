ef853e886bfd26f5aec847bfddb8a64acce0dc47
bahiyaa <biyya017@gmail.com>
2025-03-13 06:57:24 +0530

    error correcting

---
+++ FrontEnd/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='h-[100px] bg-[#e9eb77] flex items-center justify-between px-10'>
+        <img src="" alt="" />
+
+    </div>
+  )
+}
+
+export default Navbar
