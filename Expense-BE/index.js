4bd7abd8edc0ed77cc4d71fa0d54f1129048399e
bahiyaa <biyya017@gmail.com>
2025-04-14 17:57:58 +0530

    frontend url changed

--- Expense-BE/index.js
+++ Expense-BE/index.js
@@ -16,7 +16,7 @@ const adminExpenseRoute=require("./routes/expense")
 const app=express();
 // MIDDLEWARE
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://expense-tracker-gamma-three-69.vercel.app/login", "http://localhost:5174"],
     credentials: true,
     allowedHeaders: ["Authorization", "Content-Type"]
   }));
