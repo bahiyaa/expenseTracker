dfa0980b2202b6a1d42139e7520499c88909810e
bahiyaa <biyya017@gmail.com>
2025-04-22 12:12:26 +0530

    backend changed

--- Admin/src/requestMethods.js
+++ Admin/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // ✅ Base Axios Instance
-const BASE_URL = "https://expensetrackerbackend-l6jr.onrender.com/v1";
+const BASE_URL = "https://expense-tracker-bxaaujs7i-bahiyas-projects.vercel.app/v1";
 
 // ✅ Get token from localStorage
 const adminToken = sessionStorage.getItem("adminToken");
